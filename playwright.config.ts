f4b5b1ebd06fafb51b3cab324af29e1e29f2e6b9
zawawimanja <zawawimanja@gmail.com>
2024-09-03 11:14:39 +0800

    setel ilat

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 60 * 1000, // 60 seconds
   testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
