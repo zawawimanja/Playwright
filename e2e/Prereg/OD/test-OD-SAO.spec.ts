2ac2eb43a3b903c0a6403177efe188e9ee125790
zawawimanja <zawawimanja@gmail.com>
2024-09-27 09:23:42 +0800

    update file name

--- e2e/Prereg/OD/test-OD-SAO.spec.ts
+++ e2e/Prereg/OD/test-OD-SAO.spec.ts
@@ -1,21 +1,24 @@
 import { test, expect } from '@playwright/test';
-import { login } from '../../utils/base'; // Import from base.ts
-import { PreregistrationPage } from '../../pages/prereg';
-import { LeftTabPage } from '../../pages/left_tab';
-import { DraftPage } from '../../pages/draft';
-import { RemarksPage } from '../../pages/remarks';
-import { PreviewSubmissionPage } from '../../pages/preview_submission';
-import { OccupationalDiseasePage } from '../../pages/od_info';
-import { EmployerInfoPage } from '../../pages/employer_info';
-import { MedicalCertificatePage } from '../../pages/mc_info';
-import { WagesInfoPage } from '../../pages/wages_info';
-import { InsuredPersonInfoPage } from '../../pages/insured_person_info';
-import { PreferredSOCSOOfficePage } from '../../pages/socso_office';
-import { CertificationByEmployerPage } from '../../pages/cert_employer';
-import { BankInformationPage } from '../../pages/bank_info';
-import { SupportingDocumentPage } from '../../pages/support_doc';
-import { ConfirmationOfInsuredPage } from '../../pages/confirm_person';
-import { CalendarPage } from '../../utils/calendar';
+import { login } from '../../../utils/base'; // Import from base.ts
+import { PreregistrationPage } from '../../../pages/prereg';
+import { LeftTabPage } from '../../../pages/left_tab';
+import { DraftPage } from '../../../pages/draft';
+import { RemarksPage } from '../../../pages/remarks';
+import { PreviewSubmissionPage } from '../../../pages/preview_submission';
+import { OccupationalDiseasePage } from '../../../pages/od_info';
+import { EmployerInfoPage } from '../../../pages/employer_info';
+import { MedicalCertificatePage } from '../../../pages/mc_info';
+import { WagesInfoPage } from '../../../pages/wages_info';
+import { InsuredPersonInfoPage } from '../../../pages/insured_person_info';
+import { PreferredSOCSOOfficePage } from '../../../pages/socso_office';
+import { CertificationByEmployerPage } from '../../../pages/cert_employer';
+import { BankInformationPage } from '../../../pages/bank_info';
+import { SupportingDocumentPage } from '../../../pages/support_doc';
+import { ConfirmationOfInsuredPage } from '../../../pages/confirm_person';
+import { CalendarPage } from '../../../utils/calendar';
+
+// recommendation action
+
 
 
 test.beforeEach(async ({ page }) => {
