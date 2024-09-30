af9836acda4e53d7a8293424fecec98d45117791
zawawimanja <zawawimanja@gmail.com>
2024-09-30 11:23:46 +0800

    fix mc multiple date

--- e2e/Prereg/OD/test-OD-PK.spec.ts
+++ e2e/Prereg/OD/test-OD-PK.spec.ts
@@ -1,56 +1,52 @@
-import { test, expect } from '@playwright/test';
-import { login } from '../../../utils/base'; // Import from base.ts
-import { PreregistrationPage } from '../../../pages/prereg';
-import { LeftTabPage } from '../../../pages/left_tab';
-import { DraftPage } from '../../../pages/draft';
-import { RemarksPage } from '../../../pages/remarks';
-import { PreviewSubmissionPage } from '../../../pages/preview_submission';
-import { OccupationalDiseasePage } from '../../../pages/od_info';
-import { EmployerInfoPage } from '../../../pages/employer_info';
-import { MedicalCertificatePage } from '../../../pages/mc_info';
-import { WagesInfoPage } from '../../../pages/wages_info';
-import { InsuredPersonInfoPage } from '../../../pages/insured_person_info';
-import { PreferredSOCSOOfficePage } from '../../../pages/socso_office';
-import { CertificationByEmployerPage } from '../../../pages/cert_employer';
-import { BankInformationPage } from '../../../pages/bank_info';
-import { SupportingDocumentPage } from '../../../pages/support_doc';
-import { ConfirmationOfInsuredPage } from '../../../pages/confirm_person';
-import { CalendarPage } from '../../../utils/calendar';
-
+import { test, expect } from "@playwright/test";
+import { login } from "../../../utils/base"; // Import from base.ts
+import { PreregistrationPage } from "../../../pages/prereg";
+import { LeftTabPage } from "../../../pages/left_tab";
+import { DraftPage } from "../../../pages/draft";
+import { RemarksPage } from "../../../pages/remarks";
+import { PreviewSubmissionPage } from "../../../pages/preview_submission";
+import { OccupationalDiseasePage } from "../../../pages/od_info";
+import { EmployerInfoPage } from "../../../pages/employer_info";
+import { MedicalCertificatePage } from "../../../pages/mc_info";
+import { WagesInfoPage } from "../../../pages/wages_info";
+import { InsuredPersonInfoPage } from "../../../pages/insured_person_info";
+import { PreferredSOCSOOfficePage } from "../../../pages/socso_office";
+import { CertificationByEmployerPage } from "../../../pages/cert_employer";
+import { BankInformationPage } from "../../../pages/bank_info";
+import { SupportingDocumentPage } from "../../../pages/support_doc";
+import { ConfirmationOfInsuredPage } from "../../../pages/confirm_person";
+import { CalendarPage } from "../../../utils/calendar";
 
 test.beforeEach(async ({ page }) => {
   await login(page, "afzan.pks", "u@T_afzan");
 });
 
-test('Prereg PK OD', async ({ page }) => {
+test("Prereg PK OD", async ({ page }) => {
   const preregPage = new PreregistrationPage(page);
   const leftTabPage = new LeftTabPage(page);
-
-
 
   await leftTabPage.leftBar.waitFor();
   await expect(leftTabPage.leftBar).toBeVisible();
   expect(leftTabPage.pageBuilderRoot).toContainText(
-    'HomePre-RegistrationHUK Pre-RegistrationCreate RevisionMy CasesAppointmentsInsured Person SearchToolsSSNCommon ListingPermanent RepresentativeAnnual DeclarationReemployment Scheduler'
+    "HomePre-RegistrationHUK Pre-RegistrationCreate RevisionMy CasesAppointmentsInsured Person SearchToolsSSNCommon ListingPermanent RepresentativeAnnual DeclarationReemployment Scheduler"
   );
   await expect(leftTabPage.preregistrationLink).toBeVisible();
   leftTabPage.clickPreregistration();
 
-
-  await preregPage.selectNoticeTypePreRegOption('OD');
-  await preregPage.selectInsuredPersonEmployment('Yes')
-  await preregPage.selectIdentificationType('2');
-  await preregPage.fillIdentificationNo('70909085462');
-  await preregPage.selectNoticeAndBenefitClaimFormOption('Insured Person');
-  await preregPage.fillEmployerCode('B3401010420M');
+  await preregPage.selectNoticeTypePreRegOption("OD");
+  await preregPage.selectInsuredPersonEmployment("Yes");
+  await preregPage.selectIdentificationType("2");
+  await preregPage.fillIdentificationNo("801126086066");
+  await preregPage.selectNoticeAndBenefitClaimFormOption("Insured Person");
+  await preregPage.fillEmployerCode("B3401010420M");
   await preregPage.clickClaimFormSubmissionByListButton();
   await preregPage.clickSearchButton();
-  const page1Promise = page.waitForEvent('popup');
+  const page1Promise = page.waitForEvent("popup");
   await preregPage.clickNextButton();
   const page1 = await page1Promise;
 
   const draftPage = new DraftPage(page1);
-  if (await draftPage.closeButton.count() > 0) {
+  if ((await draftPage.closeButton.count()) > 0) {
     await draftPage.closeButton.waitFor();
     await draftPage.clickCloseButton();
   }
@@ -58,58 +54,70 @@ test('Prereg PK OD', async ({ page }) => {
   const remarksPage = new RemarksPage(page1);
   await remarksPage.remarksButton.waitFor();
   await expect(remarksPage.remarksButton).toBeVisible();
-  await expect(remarksPage.sectionTabs).toContainText('Remarks');
+  await expect(remarksPage.sectionTabs).toContainText("Remarks");
   await remarksPage.remarksButton.waitFor();
   await remarksPage.addRemarksButton.click();
-  await remarksPage.textbox.fill('test');
+  await remarksPage.textbox.fill("test");
   await remarksPage.saveRemarksButton.click();
 
   const insuredPersonInfoPage = new InsuredPersonInfoPage(page1);
   const calendarPage = new CalendarPage(page1);
   await insuredPersonInfoPage.clickInsuredPersonInfoButton();
-  await insuredPersonInfoPage.noticeAndBenefitClaimFormReceivedDateInput.click()
+  await insuredPersonInfoPage.noticeAndBenefitClaimFormReceivedDateInput.click();
 
-await calendarPage.selectDateInsuredPersonPage('2018', '9', '10');
-  await insuredPersonInfoPage.fillOccupation('CS');
-  await insuredPersonInfoPage.selectOccupation('1000002');
-  await insuredPersonInfoPage.selectSubOccupation('1001132');
-  await insuredPersonInfoPage.selectSubOccupationalList('1002058');
+  await calendarPage.selectDateInsuredPersonPage("2018", "9", "10");
+  await insuredPersonInfoPage.fillOccupation("CS");
+  await insuredPersonInfoPage.selectOccupation("1000002");
+  await insuredPersonInfoPage.selectSubOccupation("1001132");
+  await insuredPersonInfoPage.selectSubOccupationalList("1002058");
 
-await insuredPersonInfoPage.fillAddress1('Taman');
-  await insuredPersonInfoPage.fillAddress(2, 'Lorong 10');
-  await insuredPersonInfoPage.fillAddress(3, 'Jalan 1');
-  await insuredPersonInfoPage.selectState('200714');
-  await insuredPersonInfoPage.selectCity('201460');
-  await insuredPersonInfoPage.fillPostcode('51000');
-  await insuredPersonInfoPage.selectNationality('201749');
+  await insuredPersonInfoPage.fillAddress1("Taman");
+  await insuredPersonInfoPage.fillAddress(2, "Lorong 10");
+  await insuredPersonInfoPage.fillAddress(3, "Jalan 1");
+  await insuredPersonInfoPage.selectState("200714");
+  await insuredPersonInfoPage.selectCity("201460");
+  await insuredPersonInfoPage.fillPostcode("51000");
+  await insuredPersonInfoPage.selectNationality("201749");
 
-const employerInfoPage = new EmployerInfoPage(page1);
+  const employerInfoPage = new EmployerInfoPage(page1);
   await employerInfoPage.clickEmployerInfoButton();
 
   const occupationalDiseasePage = new OccupationalDiseasePage(page1);
   await occupationalDiseasePage.clickOccupationalDiseaseButton();
-  await occupationalDiseasePage.fillDescriptionOfOccupational('test');
+  await occupationalDiseasePage.fillDescriptionOfOccupational("test");
 
   await occupationalDiseasePage.selectDiseaseRelatedEMploymentOption("Yes");
 
-
-  await occupationalDiseasePage.fillSpecifyDutiesAndHow('test');
-  await occupationalDiseasePage.fillPleaseExplainSymptoms('test');
-const medicalCertificatePage = new MedicalCertificatePage(page1);
+  await occupationalDiseasePage.fillSpecifyDutiesAndHow("test");
+  await occupationalDiseasePage.fillPleaseExplainSymptoms("test");
+  const medicalCertificatePage = new MedicalCertificatePage(page1);
   await medicalCertificatePage.clickMedicalCertificateButton();
-
 
   //1st mc
   await medicalCertificatePage.addRecord();
-  await medicalCertificatePage.enterClinicHospitalName('kl');
+  await medicalCertificatePage.enterClinicHospitalName("kl");
 
-  await page1.getByRole('textbox').nth(1).click();
-  await calendarPage.selectDateInsuredPersonPage('2018', '8', '8');
+  await page1.getByRole("textbox").nth(1).click();
+  await calendarPage.selectDateInsuredPersonPage("2018", "8", "8");
 
-  await calendarPage.selectDateMCEndDate('2018', '9', '22');
+  await page1.getByRole("textbox").nth(2).click();
+  await calendarPage.selectDateMCEndDate("2018", "9", "22");
   await medicalCertificatePage.submitButton().click();
 
-const wagesInfoPage = new WagesInfoPage(page1);
+  //2nd mc
+  await medicalCertificatePage.addRecord();
+  await medicalCertificatePage.enterClinicHospitalName("kl");
+
+  await calendarPage.startDateInput.click();
+
+  await calendarPage.selectDateInsuredPersonPage("2018", "9", "8");
+
+  await calendarPage.endDateInput.click();
+
+  await calendarPage.selectDateMCEndDate("2018", "10", "22");
+  await medicalCertificatePage.submitButton().click();
+
+  const wagesInfoPage = new WagesInfoPage(page1);
   await wagesInfoPage.clickWagesInfoButton();
 
   const preferredSOCSOOfficePage = new PreferredSOCSOOfficePage(page1);
@@ -117,10 +125,10 @@ const wagesInfoPage = new WagesInfoPage(page1);
 
   const certificationByEmployerPage = new CertificationByEmployerPage(page1);
   await certificationByEmployerPage.clickCertificationByEmployerButton();
-  await certificationByEmployerPage.fillName('MAT');
-  await certificationByEmployerPage.fillDesignation('CEO');
+  await certificationByEmployerPage.fillName("MAT");
+  await certificationByEmployerPage.fillDesignation("CEO");
   await certificationByEmployerPage.calendar.click();
-  await calendarPage.selectDateInsuredPersonPage('2021', '7', '11');
+  await calendarPage.selectDateInsuredPersonPage("2021", "7", "11");
 
   const bankInformationPage = new BankInformationPage(page1);
   await bankInformationPage.clickBankInformationButton();
@@ -129,18 +137,16 @@ const wagesInfoPage = new WagesInfoPage(page1);
   await expect(bankInformationPage.accountNoSelect).toBeVisible();
   await bankInformationPage.accountNoSelect.click();
 
-
-  await page1.getByLabel('Account No.*', { exact: true }).selectOption('Yes');
-  await bankInformationPage.selectAccountNo('Yes');
-  await bankInformationPage.selectBankLocation('204101');
-  await bankInformationPage.selectBankName('802121');
-  await bankInformationPage.selectBankAccountType('204401');
-  await bankInformationPage.fillBankBranch('KL');
-  await bankInformationPage.fillBankAccountNo('12345678');
-
+  await page1.getByLabel("Account No.*", { exact: true }).selectOption("Yes");
+  await bankInformationPage.selectAccountNo("Yes");
+  await bankInformationPage.selectBankLocation("204101");
+  await bankInformationPage.selectBankName("802121");
+  await bankInformationPage.selectBankAccountType("204401");
+  await bankInformationPage.fillBankBranch("KL");
+  await bankInformationPage.fillBankAccountNo("12345678");
 
   const confirmationOfInsuredPage = new ConfirmationOfInsuredPage(page1);
-await confirmationOfInsuredPage.clickConfirmationOfInsuredButton();
+  await confirmationOfInsuredPage.clickConfirmationOfInsuredButton();
   await confirmationOfInsuredPage.checkCompletedCheckbox();
 
   const supportingDocumentPage = new SupportingDocumentPage(page1);
@@ -148,13 +154,9 @@ await confirmationOfInsuredPage.clickConfirmationOfInsuredButton();
 
   const previewSubmissionPage = new PreviewSubmissionPage(page1);
   await previewSubmissionPage.clickPreviewSubmissionButton();
-//   await previewSubmissionPage.clickShowPreviewButton();
-
+  //   await previewSubmissionPage.clickShowPreviewButton();
 
   // await previewSubmissionPage.clickSubmitButton();
   // await previewSubmissionPage.clickYesButton();
   // await previewSubmissionPage.navigateToEFormRenderPage();
-
-
-
 });
--- utils/calendar.ts
+++ utils/calendar.ts
@@ -1,74 +1,64 @@
-import { Page } from '@playwright/test';
-
+import { Page } from "@playwright/test";
 
 export class CalendarPage {
-    private page: Page;
+  private page: Page;
 
+  constructor(page: Page) {
+    this.page = page;
+  }
 
-    constructor(page: Page) {
-        this.page = page;
+  private get baristaPageOut() {
+    return this.page.frameLocator("#baristaPageOut");
+  }
 
-    }
+  private get uidatepicker() {
+    return this.page.locator("#ui-datepicker-div");
+  }
 
+  async selectDatePrereg(year: string, month: string, day: string) {
+    await this.baristaPageOut.getByRole("combobox").nth(1).selectOption(year);
+    //+1
+    await this.baristaPageOut.getByRole("combobox").first().selectOption(month);
 
-    private get baristaPageOut() {
-        return this.page.frameLocator('#baristaPageOut');
-    }
+    await this.page.getByRole("link", { name: day, exact: true }).click();
+  }
 
+  private mcDate() {
+    return this.page.getByRole("textbox");
+  }
+  private yearmonthMcDate() {
+    return this.page.getByRole("combobox");
+  }
 
-    private get uidatepicker() {
-        return this.page.locator('#ui-datepicker-div');
-    }
+  async selectDateInsuredPersonPage(year: string, month: string, day: string) {
+    await this.uidatepicker.getByRole("combobox").nth(1).selectOption(year);
+    //+1
+    await this.uidatepicker.getByRole("combobox").first().selectOption(month);
 
+    await this.page.getByRole("link", { name: day, exact: true }).click();
+  }
 
-      async selectDatePrereg(year: string, month: string, day: string) {
-     
-        await this.baristaPageOut.getByRole('combobox').nth(1).selectOption(year);
-        //+1
-        await this.baristaPageOut.getByRole('combobox').first().selectOption(month);
+  get startDateInput() {
+    return this.page.locator(
+      'div.subCtrlPreviewCtrlHolder[controlholdername="StartDate"] >> nth=1 >> input.datepicker'
+    );
+  }
+  // Locator for the End Date input field
+  get endDateInput() {
+    return this.page.locator(
+      'div.subCtrlPreviewCtrlHolder[controlholdername="EndDateMC"] >> nth=1 >> input.datepicker'
+    );
+  }
 
-        await this.page.getByRole('link', { name: day, exact: true }).click();
+  async selectDateMCEndDate(year: string, month: string, day: string) {
+    await this.uidatepicker.getByRole("combobox").nth(1).selectOption(year);
+    //+1
+    await this.uidatepicker.getByRole("combobox").first().selectOption(month);
 
+    await this.page.getByRole("link", { name: day, exact: true }).click();
+  }
 
-    }
-
-      private  mcDate() {
-        return this.page.getByRole('textbox')
-    }
-        private  yearmonthMcDate() {
-        return this.page.getByRole('combobox')
-    }
-
-
-
-    async selectDateInsuredPersonPage(year: string, month: string, day: string) {
-     
-        await this.uidatepicker.getByRole('combobox').nth(1).selectOption(year);
-        //+1
-        await this.uidatepicker.getByRole('combobox').first().selectOption(month);
-
-        await this.page.getByRole('link', { name: day, exact: true }).click();
-
-
-    }
-
-        async selectDateMCEndDate(year: string, month: string, day: string) {
-       
-       await this.page.getByRole('textbox').nth(2).click();
-        await this.uidatepicker.getByRole('combobox').nth(1).selectOption(year);
-        //+1
-        await this.uidatepicker.getByRole('combobox').first().selectOption(month);
-
-        await this.page.getByRole('link', { name: day, exact: true }).click();
-
-
-    }
-
-
-    async navigateCalendar(direction: 'prev' | 'next') {
-        await this.baristaPageOut.getByTitle(direction === 'prev' ? 'Prev' : 'Next', { exact: true }).click();
-
-    }
-
-
+  async navigateCalendar(direction: "prev" | "next") {
+    await this.baristaPageOut.getByTitle(direction === "prev" ? "Prev" : "Next", { exact: true }).click();
+  }
 }
